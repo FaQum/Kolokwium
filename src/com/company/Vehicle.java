69b4d2e3464f4362f095fc7e4be45c757da00f3d
Kamil Świerczyński <kspuck3@gmail.com>
2021-06-26 18:28:17 +0200

    Zad 1, Zad 2, Zad 3, Zad 4

---
+++ src/com/company/Vehicle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public abstract class Vehicle {
+    String registrationNumber;
+    String vinNumber;
+    String color;
+    Double price;
+    String combustion;
+    Double tankCondition;
+    Double mileageCounter;
+}
